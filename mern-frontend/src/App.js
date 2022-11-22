faedd200f16f624a3b9b86d91713966f1e592e59
Aisling <aisling.young4@gmail.com>
2022-11-22 16:45:06 +0000

    Added Auth Services and Context

--- mern-frontend/src/App.js
+++ mern-frontend/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React, { useContext } from "react";
+import { AuthContext } from "./Context/AuthContext";
 import "./index.css";
 import Navbar from "./components/Navigation/Navbar";
 import SearchBooks from "./components/Searchbar/SearchBooks";
 
 export default function App() {
+  const { user,setUser,isAuthenticated,setIsAuthenticated } = useContext(AuthContext);
+  console.log(user);
+  console.log(isAuthenticated);
   return (
       <div>
           <div><Navbar></Navbar></div>
--- mern-frontend/src/index.js
+++ mern-frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import App from './App';;
+import App from './App';
+import AuthProvider from './Context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider><App /></AuthProvider>
   </React.StrictMode>
 );
