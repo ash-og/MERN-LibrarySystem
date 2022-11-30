a76a09313c94d712a31e342ae37941988f866b2f
Aisling <aisling.young4@gmail.com>
2022-11-30 16:36:44 +0000

    Fixed edit button. Populated UserDetails

--- mern-frontend/src/components/users/UserDetails.js
+++ mern-frontend/src/components/users/UserDetails.js
@@ -1,14 +1,54 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // Returns read only list of users with an edit button
 
 const UserDetails = ({ user, handleEditToggle }) => {
     return (
-        <tr>
-            <td>{user.name}</td>
-            <td>{user.email}</td>
-            <td><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={(event)=> handleEditToggle (event, user)}>Edit</button></td>
-        </tr>
+        <Container>
+            <Row>
+                <Col><h1 className="text-center">Welcome to your profile</h1></Col>
+            </Row>
+            <br/>
+            <Row>
+                <Col>
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src={`${user.image}`} alt='avatar' className="img-fluid img-thumbnail" />
+                    </div>
+                </Col>
+                <Col xs={6}>
+                    <div className="card" style={{width: '25rem'}}>
+                        <div className="card-body">
+                            <Row>
+                                <Col sm="9">
+                                    <p className="card-text" >Username </p>
+                                </Col>
+                                <Col>
+                                    <p className="card-text text-muted">{user.username}</p>
+                                </Col>
+                            </Row>
+                            <hr />
+                            <Row>
+                                <Col sm="6">
+                                    <p className="card-text" >Email </p>
+                                </Col>
+                                <Col>
+                                    <p className="card-text text-muted">{user.email}</p>
+                                </Col>
+                            </Row>
+                            <hr />
+                            <Row className="justify-content-md-center">
+                                <Col className="text-center">
+                                    <button type="button" className="btn btn-dark btn-md" onClick={(event)=> handleEditToggle(event, user)}>Edit Details</button>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+      </Container>
     );
 };
 
--- mern-frontend/src/pages/Profile.js
+++ mern-frontend/src/pages/Profile.js
@@ -1,7 +1,4 @@
 import React, {useState,useContext,useEffect} from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 // import Message from '../components/Message';
 import { AuthContext } from '../Context/AuthContext';
 import UserService from '../Services/UserService';
@@ -9,7 +6,7 @@ import EditUser from '../components/users/EditUser';
 import UserDetails from '../components/users/UserDetails';
 
 const Profile = props =>{
-    const [ user, setUser ] = useState({username: "", email: ""});
+    const [ user, setUser ] = useState({username: "", email: "", image: ""});
     const [ editing, setEditing ] = useState(false);
     // const [message,setMessage] = useState(null);
     const authContext = useContext(AuthContext);
@@ -72,43 +69,22 @@ const Profile = props =>{
     };
 
     return(
-        <Container>
-        <Row>
-          <Col><h1 className="text-center">Welcome to your profile</h1></Col>
-        </Row>
-        <br/>
-        <Row>
-            <Col>
-                <div className="card" style={{ width: '18rem' }}>
-                    <img src={`${user.image}`} alt='profile photo' className="img-fluid img-thumbnail" />
-                </div>
-            </Col>
-            <Col>
-                <div className="card" style={{width: '18rem'}}>
-                    <div className="card-body">
-                        <Row>
-                            <Col sm="8">
-                                <p className="card-text" >Username </p>
-                            </Col>
-                            <Col>
-                                <p className="card-text text-muted">{user.username}</p>
-                            </Col>
-                        </Row>
-                        <hr />
-                        <Row>
-                            <Col sm="4">
-                                <p className="card-text" >Email </p>
-                            </Col>
-                            <Col>
-                                <p className="card-text text-muted">{user.email}</p>
-                            </Col>
-                        </Row>
-                    </div>
-                </div>
-            </Col>
-
-        </Row>
-      </Container>
+        <>
+        { editing ? (
+            <EditUser 
+            user={user}
+            updateUserData={updateUserData} 
+            setUpdateUserData={setUpdateUserData}  
+            setEditing={setEditing}                                       
+            /> 
+        ) : (
+            // <tr>{user.username}</tr>
+            <UserDetails 
+            user={user} 
+            handleEditToggle={handleEditToggle}
+            />
+        )}                                       
+    </>
     );
 }
 
