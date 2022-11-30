9303518260d44c8a5ff17a4126d3a05e5d5e39fd
Aisling <aisling.young4@gmail.com>
2022-11-30 22:02:31 +0000

    Heart Fave function working

--- mern-frontend/src/components/Searchbar/Table.js
+++ mern-frontend/src/components/Searchbar/Table.js
@@ -1,16 +1,42 @@
+import React, { useContext } from "react";
 import styles from './styles.module.css'
-import faveImg from '../../assets/heart.svg'
+import {AuthContext} from "../../Context/AuthContext";
 
 // Credit @ https://www.youtube.com/watch?v=_i4Wi43NTDo 
 
 const Table = ({ books }) => {
+	const {isAuthenticated} = useContext(AuthContext);
+
+	const handleHeartClick = (event, book) => {
+		event.preventDefault();
+
+		let bookId = book._id;
+
+		try {
+			fetch(`/user/favourite/${bookId}`,{
+				method : "post",
+			}).then(response=>{
+				if(response.status !== 401){
+					return response.json().then(data => data);
+				}
+				else
+					return {message : {msgBody : "UnAuthorized"},msgError : true};
+			});
+
+		} catch (err) {
+			console.log(err);
+		}};
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.headingTable}>
 				<p className={styles.title_tab}>Title</p>
                 <p className={styles.genre_tab}>Author</p>
 				<p className={styles.genre_tab}>Genre</p>
-                <p className={styles.genre_tab}>Favourite</p>
+				{ !isAuthenticated ?
+				null :
+                (<p className={styles.genre_tab}>Favourite</p>)
+				}
 			</div>
 			{books.map((book) => (
 				<div className={styles.book} key={book._id}>
@@ -25,9 +51,12 @@ const Table = ({ books }) => {
 					<div className={styles.genre_container}>
 						{book.genre}
 					</div>
-                    <div className={styles.genre_container}>
-						<img src={faveImg} alt="Heart Icon for favourites"/>
-					</div>
+					{ !isAuthenticated ? 
+					null :
+					(<div className={styles.genre_container}>
+						<button type="button" className="btn btn-outline-primary waves-effect px-2" onClick={(event)=> handleHeartClick(event, book)}><i className="bi bi-heart" aria-hidden="true"></i></button>
+					</div>)}
+
 				</div>
 			))}
 		</div>
