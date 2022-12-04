5d3cc744cebfdcf2e457cc2d6fe9a2563085d247
Aisling <aisling.young4@gmail.com>
2022-12-04 15:53:41 +0000

    Reserve FE working

--- mern-frontend/src/components/Reservations/ReservationItem.js
+++ mern-frontend/src/components/Reservations/ReservationItem.js
@@ -8,17 +8,10 @@ const ReservationItem = props =>{
         event.preventDefault();
 
 		let resId = reservation._id;
-        let bookId = {
-            id: reservation.book._id
-        };
 
 		try {
 			fetch(`/reserve/cancel/${resId}`,{
 				method : "DELETE",
-                headers: {
-                    'Content-Type': "application/json"
-                },
-                body: JSON.stringify(bookId)
 			}).then(response=>{
 				if(response.status !== 401){
 					return response.json().then(data => data);
